b8e118d23d9c6c265b7a056982b1492af543d05a
dhe3454 <dh3454@gmail.com>
2015-08-11 12:56:11 -0500

    wheel class

---
+++ src/Wheel.cpp
@@ -0,0 +1,18 @@
+/*
+ * Wheel.cpp
+ *
+ *  Created on: Aug 11, 2015
+ *      Author: Daniel
+ */
+
+#include "Wheel.h"
+
+Wheel::Wheel() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Wheel::~Wheel() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Wheel.h
@@ -0,0 +1,54 @@
+/*
+ * Wheel.h
+ *
+ *  Created on: Aug 11, 2015
+ *      Author: Daniel
+ */
+
+#ifndef WHEEL_H_
+#define WHEEL_H_
+
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Bin.h"
+
+class Wheel {
+	std::vector<Bin> bins = std::vector<Bin>(38);
+public:
+	Wheel();
+	virtual ~Wheel();
+
+	Bin getBin(int choice){
+		if( choice < 37){
+			return bins[choice];
+		}
+		else
+			return Bin();
+
+	}
+
+	void setBin(int binNum, Bin bin){
+		if( binNum < 37)
+			bins[binNum] = bin;
+	}
+
+	void addOutcome(int binChoice, Outcome addition){
+		if(binChoice > 38){
+			return;
+		}
+		else{
+			bins[binChoice].addOutcome(addition);
+		}
+	}
+
+	Bin spin(){
+
+		return bins[rand() % 38];
+	}
+
+
+};
+
+#endif /* WHEEL_H_ */
